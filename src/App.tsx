9f5992c7d087b95f7f2f028bd9ae4663a070d81b
neennera <neennera@gmail.com>
2024-05-16 23:27:01 +0700

    add: quiz flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import Quiz from "./Components/Quiz/Quiz";
+import { useState } from 'react';
+import Quiz from './Components/Quiz/Quiz';
 
 const App = () => {
-  return(
-    <main className="min-h-screen flex flex-col font-serif justify-center items-center space-x-2 space-y-2 bg-primary">
-      <div>
-        <p className="text-2xl">1 ใน 3 คำถาม ก่อนหมีจะมา</p>
-      </div>
-      <Quiz />
+  let [result, setResult] = useState("");
+
+  return (
+    <main className="min-h-screen flex flex-col font-serif justify-center items-center space-x-2 space-y-2 bg-primary">     
+      {result == "" ? <Quiz setResult={setResult} /> : <p>{result}</p>}
     </main>
-  )
-}
+  );
+};
 
 export default App;
--- src/Components/Quiz/Quiz.tsx
+++ src/Components/Quiz/Quiz.tsx
@@ -1,23 +1,68 @@
-const Quiz = () => {
-  return(
+import {useState, SetStateAction} from "react";
+import { Questions } from "../../assets/Questions";
+
+interface Props {
+  setResult: React.Dispatch<SetStateAction<string>>;
+}
+const Quiz =  (Props:Props)=> {
+  let [index, setIndex] = useState(0);
+  let [question, setQuestion] = useState(Questions[index])
+  let [huntScore, setHuntScore] = useState(0);
+  let [kindScore, setKindScore] = useState(0);
+  
+  const checkAns = (e: React.MouseEvent, ans:number) =>{
+    setHuntScore(prev => prev  + question.answer[ans].point[0])
+    setKindScore(prev => prev  + question.answer[ans].point[1])
+    next();
+  }
+
+  const next = () => {    
+    if(index === 2){
+      console.log(huntScore, kindScore);
+      
+      if(huntScore > 2){
+        Props.setResult("Hunter");
+      }else if(kindScore > 2){
+        Props.setResult("Tamer");
+      }else{
+        Props.setResult("Meat")
+      }
+    }else{
+      setIndex(++index);
+      setQuestion(Questions[index]);
+    }
+    
+  }
+
+  return (
     <div className="text-2xl w-[80vw] flex flex-col space-y-5">
-      <h1 className="text-5xl text-black ">Quiz App</h1>
-      <h2>เสือตัวนั้นจ้องประจันหน้ากับคุณ คุณจะ...</h2>
+      <div>
+        <p className="text-2xl">{index+1} ใน 3 คำถาม ก่อนหมีจะมา</p>
+      </div>
+
+      <h1 className="text-5xl text-black ">Quiz No.{index+1}</h1>
+      <h2>{question.question}</h2>
       <hr />
       <div className="w-full h-[30vh] sm:w-[95%] space-y-3 flex flex-col">
         <div className="flex flex-col space-y-4 sm:flex-row justify-between">
-          <div className="border-4 rounded-xl hover:bg-primaryDark hover:text-white  border-white w-full sm:w-[25vw] min-h-[60px] flex items-center justify-center"><p>วิ่ง</p></div>
-          <div className="border-4 rounded-xl hover:bg-primaryDark hover:text-white  border-white w-full sm:w-[25vw] min-h-[60px] flex items-center justify-center"><p>วิ่ง</p></div>
+          <div onClick={(e) => {checkAns(e,0)}} className="border-4 rounded-xl hover:bg-primaryDark hover:text-white  border-white w-full sm:w-[25vw] min-h-[60px] flex items-center justify-center cursor-pointer">
+            <p>{question.answer[0].option}</p>
+          </div>
+          <div onClick={(e) => {checkAns(e,1)}} className="border-4 cursor-pointer rounded-xl hover:bg-primaryDark hover:text-white  border-white w-full sm:w-[25vw] min-h-[60px] flex items-center justify-center">
+            <p>{question.answer[1].option}</p>
+          </div>
         </div>
         <div className="flex flex-col space-y-4 sm:flex-row justify-between">
-          <div className="border-4 rounded-xl hover:bg-primaryDark hover:text-white  border-white w-full sm:w-[25vw] min-h-[60px] flex items-center justify-center"><p>วิ่ง</p></div>
-          <div className="border-4 rounded-xl hover:bg-primaryDark hover:text-white  border-white w-full sm:w-[25vw] min-h-[60px] flex items-center justify-center"><p>วิ่ง</p></div>
+          <div onClick={(e) => {checkAns(e,2)}} className="border-4 rounded-xl hover:bg-primaryDark cursor-pointer hover:text-white  border-white w-full sm:w-[25vw] min-h-[60px] flex items-center justify-center">
+            <p>{question.answer[2].option}</p>
+          </div>
+          <div onClick={(e) => {checkAns(e,3)}} className="border-4 cursor-pointer rounded-xl hover:bg-primaryDark hover:text-white  border-white w-full sm:w-[25vw] min-h-[60px] flex items-center justify-center">
+            <p>{question.answer[3].option}</p>
+          </div>
         </div>
-        
       </div>
-      <button className="bg-white min-h-[40px] h-[6vh] rounded-2xl w-[20%]">Next</button>
     </div>
-  )
-}
+  );
+};
 
 export default Quiz;
