69e8dceb985465e14fc106e1fdb115b35248137d
neennera <neennera@gmail.com>
2024-05-16 13:40:44 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Quiz from "./Components/Quiz/Quiz";
+
+const App = () => {
+  return(
+    <div className="text-3xl text-slate-500 m-0">
+      HI
+      <Quiz />
+    </div>
+  )
+}
+
+export default App;
