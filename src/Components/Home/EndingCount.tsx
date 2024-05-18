3ab607b79cafdbc88034d0c80e667d31df29b53f
neennera <neennera@gmail.com>
2024-05-18 18:54:01 +0700

    implement ending storage

--- src/Components/Home/EndingCount.tsx
+++ src/Components/Home/EndingCount.tsx
@@ -1,29 +1,56 @@
-import { useLocalStorage } from "../../lib/useLocalStorage";
+import { useEffect, useState } from 'react';
+import { useLocalStorage } from '../../lib/useLocalStorage';
 
 const EndingCount = () => {
-  const meatLogo = document.getElementById("meatLogo")
-  const huntLogo = document.getElementById("huntLogo")
-  const kindLogo = document.getElementById("kindLogo")
-  const {addItems, getItems} = useLocalStorage("endingCount");
+  const { getItems } = useLocalStorage('endingCount');
+  const [count, setCount] = useState(3);
+  const ending = getItems();
+
+  useEffect(() => {
+    const meatLogo = document.getElementById('meatLogo');
+    const hunterLogo = document.getElementById('hunterLogo');
+    const tamerLogo = document.getElementById('tamerLogo');
+
+    setCount(3);
+
+    if (!ending['Meat']) {
+      meatLogo?.classList.add('opacity-30');
+      setCount((prev) => prev - 1);
+    }
+    if (!ending['Hunter']) {
+      hunterLogo?.classList.add('opacity-30');
+      setCount((prev) => prev - 1);
+    }
+    if (!ending['Tamer']) {
+      tamerLogo?.classList.add('opacity-30');
+      setCount((prev) => prev - 1);
+    }
+  });
 
   return (
     <div className="text-lg items-center text-center justify-center space-y-7">
       <div className="flex flex-row items-center justify-between w-[80vw] sm:w-[30vw] text-3xl">
-        <div className="flex h-[60px] w-[60px] sm:h-[70px] sm:w-[70px] bg-white rounded-full justify-center items-center">
-          <p id="meatLogo" className="opacity-60"> 🥩 </p>
+        <div
+          id="meatLogo"
+          className="flex h-[60px] w-[60px] sm:h-[70px] sm:w-[70px] bg-white rounded-full justify-center items-center"
+        >
+          <p> 🥩 </p>
         </div>
-        <div className="flex h-[60px] w-[60px] sm:h-[70px] sm:w-[70px] bg-white rounded-full justify-center items-center">
-          <p id="huntLogo" className="opacity-60"> 🏹 </p>
+        <div
+          id="hunterLogo"
+          className="flex h-[60px] w-[60px] sm:h-[70px] sm:w-[70px] bg-white rounded-full justify-center items-center"
+        >
+          <p> 🏹</p>
         </div>
-        <div className="flex h-[60px] w-[60px] sm:h-[70px] sm:w-[70px] bg-white rounded-full justify-center items-center">
-          <p id="kindLogo" className="opacity-60"> ❣️ </p>
+        <div
+          id="tamerLogo"
+          className="flex h-[60px] w-[60px] sm:h-[70px] sm:w-[70px] bg-white rounded-full justify-center items-center"
+        >
+          <p> ❣️ </p>
         </div>
       </div>
-      <p>คุณได้พบฉากจบ 0 แบบจาก 3 ฉากจบ</p>
 
-      <button className="h-20 w-10 bg-black text-white" onClick={() => {console.log(getItems());
-      }}>See</button>
-      <button className="h-20 w-10 bg-black text-white" onClick={() => {addItems('Hunter')}}>Add Hunt</button>
+      <p>คุณได้พบฉากจบ {count} แบบจาก 3 ฉากจบ</p>
     </div>
   );
 };
--- src/lib/useLocalStorage.tsx
+++ src/lib/useLocalStorage.tsx
@@ -1,35 +1,33 @@
-export const useLocalStorage = (key:string) => {
-    const addItems = (value: string) =>{
-        const prev = getItems();
-        let items;
-        if(prev === undefined){
-            items = {
-                'Meat' : false,
-                'Hunter' : false,
-                'Tamer' : false
-            }
-        }else{
-            items = prev;
- 
-        }
-        items[value] = true;
-        console.log(items);
-        
-        try {
-            window.localStorage.setItem(key, JSON.stringify(items))
-        }catch (error){
-            console.log(error);            
-        };
-    }
+export const useLocalStorage = (key: string) => {
+  const addItems = (value: string) => {
+    let item = getItems();
+    item[value] = true;
 
-    const getItems = () =>{
-        try {
-            const item = window.localStorage.getItem(key);
-            return item ? JSON.parse(item) : undefined;
-        }catch (error){
-            console.log(error);            
-        };
+    try {
+      window.localStorage.setItem(key, JSON.stringify(item));
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return {addItems, getItems};
-}
+  const getItems = () => {
+    try {
+      const item = window.localStorage.getItem(key);
+      if (item === 'undefined' || item === null) {
+        const newItems = {
+          Meat: false,
+          Hunter: false,
+          Tamer: false,
+        };
+        window.localStorage.setItem(key, JSON.stringify(newItems));
+        return newItems;
+      } else {
+        return JSON.parse(item);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { addItems, getItems };
+};
